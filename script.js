372084ee635cccd59ae09157e45a7c4882877bb8
Juanto77 <58269289+Juanto77@users.noreply.github.com>
2022-02-11 13:07:59 -0600

    Created dynamic cell sizing

--- script.js
+++ script.js
@@ -1,44 +1,60 @@
-const gridContainer = document.getElementById('game');
-let number = 10;
+const gridContainer = document.getElementById("game");
+let number = userInput();
+size = 960 / number;
 
-//Ask user
+function userInput() {
+  let input = prompt("how many sides?", "");
+  return input;
+}
 
 //Create a grid of divs
-function makeGrid(number){
-    for (let i = 0; i < number; i++) {
-        let row = document.createElement('div')
-        gridContainer.appendChild(row);
+function makeGrid(number) {
+  for (let i = 0; i < number; i++) {
+    let row = document.createElement("div");
+    gridContainer.appendChild(row);
 
-        for (let j = 0; j < number; j++){
-        let cell = document.createElement('div')
-        row.appendChild(cell);
-        }
-
-        }
-
-};
+    for (let j = 0; j < number; j++) {
+      let cell = document.createElement("div");
+      row.appendChild(cell);
+    }
+  }
+  setSize();
+}
 
 // Build a nodelist of the Divs
-gameDiv = () => gridContainer.querySelectorAll('div');
+gameDiv = () => gridContainer.querySelectorAll("div");
 
 //Color the Grid
-gridContainer.addEventListener('mouseover', function(hover){
-    hover.target.classList.add('onHover');
+gridContainer.addEventListener("mouseover", function (hover) {
+  hover.target.classList.add("onHover");
 });
 
-//Clear Div Cells
-function resetDivs(){
-    gameDiv().forEach(function(e){
-        e.classList.remove('onHover');
-    })
+//Set the size of the grid
+function setSize() {
+  gameDiv().forEach(function (e) {
+    e.style.height = size + "px";
+    e.style.width = size + "px";
+  });
+}
 
+//Clear Div Cells
+function resetDivs() {
+  gameDiv().forEach(function (e) {
+    e.classList.remove("onHover");
+  });
 }
 
 //Clear the Grid of all Divs
-function reset(){
-    if (gridContainer.hasChildNodes()){
-        while (gridContainer.firstChild) {
-            gridContainer.removeChild(gridContainer.firstChild);
-        }
+function reset() {
+  if (gridContainer.hasChildNodes()) {
+    while (gridContainer.firstChild) {
+      gridContainer.removeChild(gridContainer.firstChild);
     }
-};
+    gameDiv().forEach(function (e) {
+      e.style.height = "";
+      e.style.width = "";
+    });
+  }
+}
+
+makeGrid(number);
